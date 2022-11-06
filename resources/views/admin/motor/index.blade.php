346f34b301875518ca6a2124f49861fe11baf2e6
MUSHANIF <hanifmusthafa2005@gmail.com>
2022-11-06 11:31:43 +0700

    menambahkan offcanvas bootstrap untuk detail motor asek

--- resources/views/admin/motor/index.blade.php
+++ resources/views/admin/motor/index.blade.php
@@ -36,6 +36,7 @@
         
             <th>Plat nomor </th>
             <th>Status</th>
+            
             <th>Action</th>
           </tr>
         </thead>
@@ -51,21 +52,29 @@
            
             <td>{{ $key->plat_nomor }}</td>
             <td>{{ $key->status }}</td>
+            
             <td>
               <div class="dropdown">
                 <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown">
                   <i class="bx bx-dots-vertical-rounded"></i>
                 </button>
                 <div class="dropdown-menu">
+                  <a class="dropdown-item" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
+                    <i class="bx bx-edit-alt me-1"></i>  Detail
+                  </a>
                   <a class="dropdown-item" href="{{ route('motor.edit',$key->id) }}"
-                    ><i class="bx bx-edit-alt me-1"></i> Edit</a
-                  >
+                    ><i class="bx bx-edit-alt me-1"></i> Edit</a>
+                    
                   <form action="{{ url('motor/'.$key->id) }}" method="POST" >
                     @csrf
                     <input type="hidden" name="_method" value="DELETE">
                     <button type="submit" class="dropdown-item"><i class="bx bx-trash me-1"></i>Delete</button>
                     
                 </form>
+                
+             
+                
+                
                   
                 </div>
               </div>
@@ -73,6 +82,46 @@
           </tr>
          
         </tbody>
+        <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+          <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasExampleLabel">Detail motor {{  $key->name }}</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+          <div class="offcanvas-body">
+            <div class="text-center mb-4">
+              <a href="" data-bs-toggle="modal" data-bs-target="#exampleModal1"><img src="/assets/images/motor/{{ $key->image }}" style="height: 100px; width: 150px" /></a>
+            </div>
+            <div class="modal fade" id="exampleModal1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+              <div class="modal-dialog modal-xl">
+                 <div class="modal-content">
+                    <div class="modal-header">
+                       <h5 class="modal-title text-center justify-content-center" id="exampleModalLabel">Detail Foto</h5>
+                       <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                       <img src="/assets/images/motor/{{ $key->image }}" style="height: 100%; width: 100%" />
+                    </div>
+                 </div>
+              </div>
+           </div>
+            <div>
+             <b>Nama motor: </b>
+             <p>{{ $key->name }}</p>
+            </div>
+            <div>
+              <b>Jenis motor: </b>
+              <p>{{  $key->motor->name }}</p>
+             </div>
+             <div>
+              <b>Plat motor: </b>
+              <p>{{ $key->plat_nomor }}</p>
+             </div>
+             <div>
+              <b>Deskripsi: </b>
+              <p>{{ $key->deskripsi }}</p>
+             </div>
+          </div>
+        </div>
         @endforeach
       </table>
     </div>
